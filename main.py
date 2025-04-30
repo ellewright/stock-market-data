8d97cb40b982569cb7001f185e5efa3a6567c017
Eleanor Wright <elliewright055@gmail.com>
2025-04-30 15:06:52 -0500

    Begin work on get_eod_data()

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
+
+def get_eod_data(ticker):
+    url = f"http://api.marketstack.com/v2/eod?access_key={API_KEY}&symbols={ticker}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"][0]
+
 def main():
-    pass
+    apple_stock_data = get_eod_data("AAPL")
+    
+    print(apple_stock_data["open"])
+    print(apple_stock_data["high"])
+    print(apple_stock_data["low"])
+    print(apple_stock_data["close"])
 
 if __name__ == "__main__":
     main()
